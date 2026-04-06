afb4405dd227b4575eac14bb19ebdf8aa9ea5689
Nartaiw <161725551+Nartaiw@users.noreply.github.com>
2026-04-06 21:48:22 +0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,16 @@
-from aiohttp import web
-import asyncio
+from flask import Flask
+from threading import Thread
 import os
+app = Flask('')
 
-async def handle(request):
-    return web.Response(text="I am alive!")
+@app.route('/')
+def home():
+    return "I am alive!"
 
-async def start_server():
-    app = web.Application()
-    app.router.add_get('/', handle)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    
-    # Render берген портты немесе 10000 портты қолданамыз
-    port = int(os.environ.get("PORT", 10000))
-    site = web.TCPSite(runner, '0.0.0.0', port)
-    await site.start()
-    print(f"Web server started on port {port}")
+def run():
+port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
